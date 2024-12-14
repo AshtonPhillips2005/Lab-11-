7983c5e595984e06a20fa635eb7d48bc3d2cb3a6
AshtonPhillips2005 <ashtongamer11@gmail.com>
2024-12-13 21:08:09 -0500

    Added methods: getTotalValue() and toString() to ProductProperties

--- script.js
+++ script.js
@@ -5,5 +5,13 @@ class ProductProperties {
         this.quantity = quantity;
     }
 
-    
+    // Method to calculate total value of the product
+    getTotalValue() {
+        return this.price * this.quantity;
+    }
+
+    // Method to display product details as a string
+    toString() {
+        return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`;
+    }
 }
